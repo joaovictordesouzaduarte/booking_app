39d586744b68ef4cc69cde1c9336a8c9d9709e75
joaovictordesouzaduarte <victorduarte.ufrj@gmail.com>
2023-01-06 08:09:08 -0300

    hotel routes

--- api/db/schemas.py
+++ api/db/schemas.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel, Field
-from typing import List
+from typing import List, Optional
 
-class Hotels(BaseModel):
+class Hotel(BaseModel):
     name: str
     type: str
     city: str
@@ -14,3 +14,17 @@ class Hotels(BaseModel):
     rooms: List[str]
     cheapestPrice: int
     featured: bool
+
+class UpdateHotel(BaseModel):
+    name: Optional[str]=None
+    type: Optional[str]=None
+    city: Optional[str]=None
+    address: Optional[str]=None
+    distance: Optional[str]=None
+    photos: Optional[List[str]]=None
+    title: Optional[str]=None
+    desc: Optional[str]=None
+    rating: int = Field(None, gt=0, le=5)
+    rooms: Optional[List[str]]=None
+    cheapestPrice: Optional[int]=None
+    featured: Optional[bool]=None
--- api/routers/hotels.py
+++ api/routers/hotels.py
@@ -1,15 +1,15 @@
 from fastapi import APIRouter, HTTPException, Depends, status
 from db.database import hotels_collection
-from db.schemas import Hotels
+from db.schemas import Hotel, UpdateHotel
 from bson.objectid import ObjectId
-
+from pymongo import ReturnDocument
 router = APIRouter(
     prefix='/hotels',
     tags = ['hotels']
 )
 
 @router.post('/')
-async def create_hotel(hotels: Hotels):
+async def create_hotel(hotels: Hotel):
     
     try:
         hotels_collection.insert_one(hotels.dict())
@@ -17,15 +17,57 @@ async def create_hotel(hotels: Hotels):
         raise HTTPException(status_code=404, detail=str(ex))
 
 @router.put('/{id}')
-async def update_hotel(id: str, hotels: Hotels):
+async def update_hotel(id: str, hotel:UpdateHotel):
     
     if not ObjectId.is_valid(id):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"Invalid id {id}")
 
     try:
-        update_hotel = hotels_collection.find_one_and_update(
-            {'_id': ObjectId(id)}, {'$set': hotels.dict(exclude=None)}
+        hotels = hotels_collection.find_one_and_update(
+            {'_id': ObjectId(id)}, {'$set': hotel.dict(exclude_none = True)}, return_document=ReturnDocument.AFTER
         )
+        del hotels['_id']
+        return hotels
+    except Exception as ex:
+        raise HTTPException(status_code=404, detail=str(ex))
+
+@router.delete('/{id}')
+async def delete_hotel(id: str):
+    
+    if not ObjectId.is_valid(id):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Invalid id {id}")
+
+    try:
+        hotels_collection.find_one_and_delete({'_id': ObjectId(id)})
+    except Exception as ex:
+        raise HTTPException(status_code=404, detail=str(ex))
+
+@router.get('/{id}')
+async def get_hotel_by_id(id: str):
+    
+    if not ObjectId.is_valid(id):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Invalid id {id}")
+    hotel = None
+    try:
+        hotel = hotels_collection.find_one({'_id': ObjectId(id)})
+        del hotel['_id']
+        return hotel
+    except Exception as ex:
+        raise HTTPException(status_code=404, detail=str(ex))
+
+@router.get('/')
+async def get_hotels():
+
+    hotels = {}
+    try:
+        data = hotels_collection.find()
+        for hotel in data:
+            hotels.update(hotel)
+        
+        del hotels['_id']
+        return hotels
     except Exception as ex:
         raise HTTPException(status_code=404, detail=str(ex))
